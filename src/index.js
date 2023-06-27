1740b9917be26122e11851c63bbeeda793be97ba
hd-buddy <117499276+hd-buddy@users.noreply.github.com>
2023-06-27 22:06:19 +0530

    basic version of robo profile

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import 'tachyons'
+import Card from './Card'
 import reportWebVitals from './reportWebVitals';
+import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+    <Card id={robots[0].id} name={robots[0].name}  email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name}  email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name}  email={robots[2].email}/>
+    <Card id={robots[3].id} name={robots[3].name}  email={robots[3].email}/>
+    </div>
   </React.StrictMode>
 );
 
